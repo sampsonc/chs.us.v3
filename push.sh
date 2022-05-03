426108f2b0f3ca10ed0bd770226dbc7988f383eb
Carl Sampson <carl.sampson@gmail.com>
2022-05-03 15:28:56 -0400

    Fixes

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 #/Library/Frameworks/Python.framework/Versions/3.8/bin/python3 /Users/chs/scripts/pocket-wp-template.py golang "Interesting Go Links" > /Users/chs/Documents/chs.us/content/go.html
 hugo
 hugo --minify
-rsync -avz --delete public/ chs@chs.us:chs.us
+rsync -avz --delete public/ chs@chs.us:chs.us/
